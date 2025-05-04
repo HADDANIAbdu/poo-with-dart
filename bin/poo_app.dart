34a79fc634a4a2e58b8a13f884b11e1a45bd6ace
HADDANIAbdu <ab.haddani10@gmail.com>
2025-05-04 23:33:42 +0100

    tester l'héritage et le polymorphisme dans la classe main

--- bin/poo_app.dart
+++ bin/poo_app.dart
@@ -1,5 +1,14 @@
-
+import 'package:poo_app/Employe.dart';
+import 'package:poo_app/Manager.dart';
 
 void main() {
-  print('Hello, World!');
+  Employe employe = Employe(name: 'John', salaire: 1000);
+  Manager manager = Manager(name: 'Jane', salaire: 2000, prime: 100);
+
+  List<Employe> listEmploye = [employe, manager];
+
+  for (Employe employe in listEmploye) {
+    employe.afficherInfos();
+    print("------------------");
+  }
 }
